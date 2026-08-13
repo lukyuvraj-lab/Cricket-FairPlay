633aee2756df512704d13c3ba969b9db27e9912b
lukyuvraj-lab <lukyuvraj@gmail.com>
2026-08-13 01:49:52 +0000

    Add trained cricket ball YOLO model

--- training/train.py
+++ training/train.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
 
 model.train(
-    data="data.yaml",
-    epochs=50,
-    imgsz=640,
-    batch=8,
-    name="cricket_ball"
-)
+    data="training/data.yaml",
+        epochs=50,
+            imgsz=640,
+                batch=8,
+                    name="cricket_ball"
+                    )
